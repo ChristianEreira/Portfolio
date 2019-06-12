c54d9e0ad347429e588cb324b2c909b839d6f066
Christian Ereira <contact@chrise.dev>
2019-06-12 15:07:39 +0100

    Animated mobile menu

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-function toggleMenu(){
-    document.getElementById("mobileMenu").classList.toggle("hidden");
-}
+function toggleMenu() {
+    if ($("#mobileMenu").is(":hidden")) {
+        $("#mobileMenu").show(400);
+    } else {
+        $("#mobileMenu").hide(400);
+    }
+};
